a2356071a24714afa77b0abd97e80675b46334cb
Grant Shively <gshively11@gmail.com>
2014-08-28 20:37:49 -0700

    Added validation for empty value property on config item, instead of just undefined

--- test/validate-item.tests.js
+++ test/validate-item.tests.js
@@ -22,6 +22,10 @@ describe('validate-item', function(){
     var result = sut({value: void(0)}, 'somekey');
     expect(result.length).to.equal(1);
   });
+  it('returns error if the value of value is null', function(){
+    var result = sut({value: null}, 'somekey');
+    expect(result.length).to.equal(1);
+  });
   it('returns empty array for valid config item', function(){
     var result = sut({value: 'test value'}, 'somekey');
     expect(result).to.be.empty;
